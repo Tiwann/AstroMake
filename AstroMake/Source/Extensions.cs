2c3ec2fec4ef0a3f172d8e687bb50ae4d272c08a
Erwann Messoah <tiwannbeats@gmail.com>
2023-06-15 11:19:54 +0200

    Create Extensions.cs

---
+++ AstroMake/Source/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Extensions
+{
+    public static String VisualCXXProject => ".vcxproj";
+    public static String VisualCXXSolution => ".sln";
+    public static String VisualCSharpProject => ".csproj";
+    public static String Makefile => "Makefile";
+    public static String XCodeProject => ".xcodeproj";
+}
