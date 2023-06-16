ebab5b216eaf5e89a7923feaeb1dcb2e2b5cfb69
Erwann Messoah <tiwannbeats@gmail.com>
2023-06-16 12:01:50 +0200

    Removed constructor

--- AstroMake/Source/Attributes.cs
+++ AstroMake/Source/Attributes.cs
@@ -5,16 +5,5 @@ namespace AstroMake;
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
 public class BuildAttribute : Attribute
 {
-    public BuildAttribute()
-    {
-    }
-}
-
-[AttributeUsage(AttributeTargets.Class)]
-public class WorkspaceAttribute : Attribute
-{
-    public WorkspaceAttribute()
-    {
-        
-    }
+    
 }
