10c6889a6ca31c6c4480b4a11cf0ba18fa938f8b
Erwann Messoah <tiwannbeats@gmail.com>
2023-06-27 15:37:22 +0200

    Added helper function to get the last element of a list

--- AstroMake/Source/Extenders.cs
+++ AstroMake/Source/Extenders.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AstroMake;
 
-public static class Externders
+public static class Extenders
 {
     public static bool IsSubclassOf<T>(this Type Type)
     {
@@ -12,5 +14,11 @@ public static class Externders
     public static T CreateInstance<T>(this Type Type, params dynamic[] Parameters) where T : class
     {
         return Activator.CreateInstance(Type, Parameters) as T;
+    }
+
+    public static T Last<T>(this IEnumerable<T> List) where T : class
+    {
+        var Temp = List.ToList();
+        return Temp.Count != 0 ? Temp[Temp.Count - 1] : null;
     }
 }
