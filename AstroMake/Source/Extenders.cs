4c7f71d91b490e54d11ac8ab13d781df104ed2d2
Erwann Messoah <tiwannbeats@gmail.com>
2023-07-05 11:45:28 +0200

    Update Extenders.cs
    Found a utility function to get a relative path

--- AstroMake/Source/Extenders.cs
+++ AstroMake/Source/Extenders.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace AstroMake;
@@ -25,5 +26,52 @@ public static class Extenders
     public static bool IsEmpty<T>(this IEnumerable<T> List)
     {
         return List.Count() == 0;
+    }
+
+    public static bool Contains<T>(this T TheEnum, T Value) where T : Enum
+    {
+        return Convert.ToBoolean(Convert.ToInt32(TheEnum) & Convert.ToInt32(Value));
+    }
+    
+    public static string GetRelativePath(this string FromPath, string ToPath)
+    {
+        if (string.IsNullOrEmpty(FromPath))
+        {
+            throw new ArgumentNullException($"{FromPath}");
+        }
+
+        if (string.IsNullOrEmpty(ToPath))
+        {
+            throw new ArgumentNullException($"{ToPath}");
+        }
+
+        Uri FromUri = new Uri(AppendDirectorySeparatorChar(FromPath));
+        Uri ToUri = new Uri(AppendDirectorySeparatorChar(ToPath));
+
+        if (FromUri.Scheme != ToUri.Scheme)
+        {
+            return ToPath;
+        }
+
+        Uri RelativeUri = FromUri.MakeRelativeUri(ToUri);
+        string RelativePath = Uri.UnescapeDataString(RelativeUri.ToString());
+
+        if (string.Equals(ToUri.Scheme, Uri.UriSchemeFile, StringComparison.OrdinalIgnoreCase))
+        {
+            RelativePath = RelativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+        }
+
+        return RelativePath;
+    }
+
+    private static string AppendDirectorySeparatorChar(string path)
+    {
+        if (!Path.HasExtension(path) &&
+            !path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+        {
+            return path + Path.DirectorySeparatorChar;
+        }
+
+        return path;
     }
 }
