616874ea6fb2bcd600d0bfec8ac69de5dde0b9af
Erwann Messoah <tiwannbeats@gmail.com>
2023-07-05 11:42:24 +0200

    Cleaned up the writer
    Added some private functions to improve readability

--- AstroMake/Source/Writers/VcxprojWriter.cs
+++ AstroMake/Source/Writers/VcxprojWriter.cs
@@ -27,6 +27,27 @@ public class VcxprojWriter : IDisposable
             WriteAttribute(Attribute.Item1, Attribute.Item2);
         }
     }
+    
+    private void WriteElement(string Name, (string, string) Attribute, params Action[] Content)
+    {
+        Writer.WriteStartElement(Name);
+        WriteAttribute(Attribute.Item1, Attribute.Item2);
+        foreach (Action Action in Content)
+        {
+            Action.Invoke();
+        }
+        Writer.WriteEndElement();
+    }
+    
+    private void WriteElement(string Name, params Action[] Content)
+    {
+        Writer.WriteStartElement(Name);
+        foreach (Action Action in Content)
+        {
+            Action.Invoke();
+        }
+        Writer.WriteEndElement();
+    }
 
     private void WriteAttribute(string Name, string Value)
     {
@@ -56,6 +77,50 @@ public class VcxprojWriter : IDisposable
         Writer.WriteEndElement();
         
     }
+    
+    private void WriteProperty(string Name, CPPStandard Standard)
+    {
+        Writer.WriteStartElement(Name);
+        Writer.WriteString(GetStandard(Standard));
+        Writer.WriteEndElement();
+    }
+    
+    private void WriteProperty(string Name, OutputType Type)
+    {
+        Writer.WriteStartElement(Name);
+        Writer.WriteString(GetConfigType(Type));
+        Writer.WriteEndElement();
+    }
+
+    private void WriteConfigurations(Solution Solutiuon)
+    {
+        WriteElement("ItemGroup", ("Label", "ProjectConfigurations"), delegate
+        {
+            foreach (Configuration Configuration in Solutiuon.Configurations)
+            {
+                if (Solutiuon.Platforms.Count > 0)
+                {
+                    foreach (string Platform in Solutiuon.Platforms)
+                    {
+                        string ConfigName = $"{Configuration.Name} {Platform}";
+                        WriteStartElement("ProjectConfiguration", ("Include", $"{ConfigName}|{Solutiuon.Architecture}"));
+                        WriteProperty("Configuration", ConfigName);
+                        WriteProperty("Platform", $"{Solutiuon.Architecture}");
+                        Writer.WriteEndElement();
+                    }
+                }
+                else
+                {
+                    string ConfigName = $"{Configuration.Name}";
+                    WriteStartElement("ProjectConfiguration", ("Include", $"{ConfigName}|{Solutiuon.Architecture}"));
+                    WriteProperty("Configuration", ConfigName);
+                    WriteProperty("Platform", $"{Solutiuon.Architecture}");
+                    Writer.WriteEndElement();
+                }
+            }
+        });
+    }
+    
 
     private string GetConfigType(OutputType Type)
     {
@@ -68,6 +133,25 @@ public class VcxprojWriter : IDisposable
                 return "SharedLibrary";
             case OutputType.StaticLibrary:
                 return"StaticLibrary";
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private string GetStandard(CPPStandard Standard)
+    {
+        switch (Standard)
+        {
+            case CPPStandard.CPP20:
+                return "stdcpp20";
+            case CPPStandard.CPP17:
+                return "stdcpp17";
+            case CPPStandard.CPP14:
+                return "stdcpp14";
+            case CPPStandard.CPP11:
+                return "stdcpp11";
+            case CPPStandard.CPPLatest:
+                return "stdcpplatest";
             default:
                 throw new ArgumentOutOfRangeException();
         }
@@ -104,126 +188,78 @@ public class VcxprojWriter : IDisposable
             // Document begin
             Writer.WriteStartDocument();
             Writer.WriteComment($"Astro Make {Version.AstroVersion} generated vcxproj");
-            Writer.WriteComment("Astro Make © Erwann Messoah 2023");
+            Writer.WriteComment("Astro Make (C) Erwann Messoah 2023");
             Writer.WriteComment("\"https://github.com/Tiwann/AstroMake\"");
 
             WriteStartElement("Project", ("DefaultTargets", "Build"), ("xmlns", XmlStatics.XmlNamespace));
-            WriteStartElement("ItemGroup", ("Label", "ProjectConfigurations"));
-            foreach (Configuration Configuration in Project.Solution.Configurations)
-            {
-                if (Project.Solution.Platforms.Count > 0)
-                {
-                    foreach (string Platform in Project.Solution.Platforms)
-                    {
-                        string ConfigName = $"{Configuration.Name} {Platform}";
-                        WriteStartElement("ProjectConfiguration", ("Include", $"{ConfigName}|{Project.Solution.Architecture}"));
-                        WriteProperty("Configuration", ConfigName);
-                        WriteProperty("Platform", $"{Project.Solution.Architecture}");
-                        Writer.WriteEndElement();
-                    }
-                }
-                else
-                {
-                    string ConfigName = $"{Configuration.Name}";
-                    WriteStartElement("ProjectConfiguration", ("Include", $"{ConfigName}|{Project.Solution.Architecture}"));
-                    WriteProperty("Configuration", ConfigName);
-                    WriteProperty("Platform", $"{Project.Solution.Architecture}");
-                    Writer.WriteEndElement();
-                }
-            }
-            Writer.WriteEndElement();
-
+            WriteConfigurations(Project.Solution);
+            
             // Write globals
-            WriteStartElement("PropertyGroup", ("Label", "Globals"));
-            WriteProperty("ProjectGuid", $"{{{Project.Guid}}}");
-            WriteProperty("ConfigurationType", GetConfigType(Project.Type));
-            WriteProperty("CharacterSet", "Unicode");
-            WriteProperty("PlatformToolset", "v143");
-            WriteProperty("WindowsTargetPlatformVersion", "10.0");
-            WriteProperty("RootNamespace", Project.Name);
-            WriteProperty("IgnoreWarnCompileDuplicatedFilename", true);
-            WriteProperty("Keyword", "Win32Proj");
-            WriteProperty("OutDir", Project.BinariesDirectory.EndsWith("\\") ? Project.BinariesDirectory : $"{Project.BinariesDirectory}\\");
-            WriteProperty("IntDir", Project.IntermediateDirectory.EndsWith("\\") ? Project.IntermediateDirectory : $"{Project.IntermediateDirectory}\\");
-            Writer.WriteEndElement();
-            
-            Writer.WriteStartElement("ItemDefinitionGroup");
-            Writer.WriteStartElement("ClCompile");
-            Writer.WriteStartElement("LanguageStandard");
-            string std;
-            switch (Project.Dialect)
+            WriteElement("PropertyGroup", ("Label", "Globals"), delegate
             {
-                case Dialect.CPP20:
-                    std = "stdcpp20";
-                    break;
-                case Dialect.CPP17:
-                    std = "stdcpp17";
-                    break;
-                case Dialect.CPP14:
-                    std = "stdcpp14";
-                    break;
-                case Dialect.CPP11:
-                    std = "stdcpp11";
-                    break;
-                case Dialect.CPPLatest:
-                    std = "stdcpplatest";
-                    break;
-                case Dialect.CSharp8:
-                case Dialect.CSharp9:
-                case Dialect.CSharp10:
-                case Dialect.CSharp11:
-                case Dialect.CSharpLatest:
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            Writer.WriteString(std);
-            Writer.WriteEndElement();
+                WriteProperty("ProjectGuid", Project.Guid);
+                WriteProperty("ConfigurationType", Project.Type);
+                WriteProperty("CharacterSet", "Unicode");
+                WriteProperty("PlatformToolset", "v143");
+                WriteProperty("WindowsTargetPlatformVersion", "10.0");
+                WriteProperty("RootNamespace", Project.Name);
+                WriteProperty("IgnoreWarnCompileDuplicatedFilename", true);
+                WriteProperty("Keyword", "Win32Proj");
+                WriteProperty("OutDir", Project.BinariesDirectory.EndsWith("\\") ? Project.BinariesDirectory : $"{Project.BinariesDirectory}\\");
+                WriteProperty("IntDir", Project.IntermediateDirectory.EndsWith("\\") ? Project.IntermediateDirectory : $"{Project.IntermediateDirectory}\\");
+            });
             
-            // Enable modules if c++20
-            if (Convert.ToBoolean((int)(Project.Flags & ProjectFlags.ModuleSupport)) && Project.Dialect is Dialect.CPP20)
+            WriteElement("ItemDefinitionGroup", ("Label", "Globals"), delegate
             {
-                WriteProperty("EnableModules", true);
-            }
+                WriteElement("ClCompile", delegate
+                {
+                    WriteProperty("LanguageStandard", Project.CppStandard);
             
-            // Enable multiprocessor compile
-            if (Convert.ToBoolean((int)(Project.Flags & ProjectFlags.MultiProcessorCompile)))
-            {
-                WriteProperty("MultiProcessorCompilation", true);
-            }
+                    // Enable modules if c++20 or above
+                    if (Project.Flags.Contains(ProjectFlags.ModuleSupport) && Project.CppStandard >= CPPStandard.CPP20)
+                    {
+                        WriteProperty("EnableModules", true);
+                    }
             
-            Writer.WriteEndElement();
-            Writer.WriteEndElement();
+                    // Enable multiprocessor compile
+                    if (Project.Flags.Contains(ProjectFlags.MultiProcessorCompile))
+                    {
+                        WriteProperty("MultiProcessorCompilation", true);
+                    }
+                });
+            });
+            
 
+            // Import Microsoft targets and props
             Writer.WriteComment("Import Microsoft targets");
-            Writer.WriteStartElement("ImportGroup");  
-            WriteStartElement("Import", ("Project", @"$(VCTargetsPath)\Microsoft.Cpp.Default.props"));
-            Writer.WriteEndElement();
-            WriteStartElement("Import", ("Project", @"$(VCTargetsPath)\Microsoft.Cpp.props"));
-            Writer.WriteEndElement();
-            WriteStartElement("Import", ("Project", @"$(VCTargetsPath)\Microsoft.Cpp.targets"));
-            Writer.WriteEndElement();
-            Writer.WriteEndElement();
+            WriteElement("ImportGroup", delegate
+            {
+                WriteElement("Import", ("Project", @"$(VCTargetsPath)\Microsoft.Cpp.Default.props"));
+                WriteElement("Import", ("Project", @"$(VCTargetsPath)\Microsoft.Cpp.props"));
+                WriteElement("Import", ("Project", @"$(VCTargetsPath)\Microsoft.Cpp.targets"));
+            });
             
             if (!Project.Links.IsEmpty())
             {
                 Writer.WriteComment("Project references");
-                Writer.WriteStartElement("ItemGroup");
-                List<Project> Projects = Project.Solution.Projects.Where(P => Project.Links.Contains(P.Name)).ToList();
-                if (!Projects.IsEmpty())
+                WriteElement("ItemGroup", delegate
                 {
-                    foreach (Project Proj in Projects)
+                    List<Project> Projects = Project.Solution.Projects.Where(P => Project.Links.Contains(P.Name)).ToList();
+                    if (!Projects.IsEmpty())
                     {
-                        WriteStartElement("ProjectReference", ("Include", @$"{Proj.TargetPath}"));
-                        WriteProperty("ProjectReference", Proj.Guid);
-                        Writer.WriteEndElement();
+                        foreach (Project Proj in Projects)
+                        {
+                            WriteStartElement("ProjectReference", ("Include", @$"{Proj.TargetPath}"));
+                            WriteProperty("ProjectReference", Proj.Guid);
+                            Writer.WriteEndElement();
+                        }
                     }
-                }
-                Writer.WriteEndElement();
+                });
             }
             
-
+            // TODO: Better files including handling
             List<string> Files = new List<string>();
-            Files.AddRange(Project.Files.Where(F => File.Exists($"{Project.Location}/{F}")));
+            Files.AddRange(Project.Files.Where(F => File.Exists(@$"{Project.Location}\{F}")));
             Project.Files.ForEach(S =>
             {
                 var Found = GetFilesFromWildcard(S);
@@ -231,7 +267,7 @@ public class VcxprojWriter : IDisposable
             });
 
             List<string> AdditionalFiles = new List<string>();
-            Files.AddRange(Project.AdditionalFiles.Where(F => File.Exists($"{Project.Location}/{F}")));
+            Files.AddRange(Project.AdditionalFiles.Where(F => File.Exists(@$"{Project.Location}\{F}")));
             Project.AdditionalFiles.ForEach(S =>
             {
                 var Found = GetFilesFromWildcard(S);
@@ -245,48 +281,48 @@ public class VcxprojWriter : IDisposable
             {
                 // Sources
                 Writer.WriteComment("Including source files");
-                Writer.WriteStartElement("ItemGroup");
-                foreach (string File in Files)
+                WriteElement("ItemGroup", ("Label", "Sources"), delegate
                 {
-                    if (AvailableSourceExtensions.Any(E => File.EndsWith(E)))
+                    foreach (string Source in Files)
                     {
-                        Writer.WriteStartElement("ClCompile");
-                        WriteAttribute("Include", File);
-                        Writer.WriteEndElement();
+                        if (AvailableSourceExtensions.Any(E => Source.EndsWith(E)))
+                        {
+                            Writer.WriteStartElement("ClCompile");
+                            WriteAttribute("Include", Project.TargetDirectory.GetRelativePath(Path.Combine(Project.Location, Source)));
+                            Writer.WriteEndElement();
+                        }
                     }
-                }
-
-                Writer.WriteEndElement();
+                });
 
                 // Headers
                 Writer.WriteComment("Including header files");
-                Writer.WriteStartElement("ItemGroup");
-                foreach (string Source in Files)
+                WriteElement("ItemGroup", ("Label", "Headers"), delegate
                 {
-                    if (AvailableHeaderExtensions.Any(E => Source.EndsWith(E)) && File.Exists(Source))
+                    foreach (string Header in Files)
                     {
-                        Writer.WriteStartElement("ClInclude");
-                        WriteAttribute("Include", Source);
-                        Writer.WriteEndElement();
+                        if (AvailableHeaderExtensions.Any(E => Header.EndsWith(E)) && File.Exists(Header))
+                        {
+                            Writer.WriteStartElement("ClInclude");
+                            WriteAttribute("Include", Path.GetFullPath(Header));
+                            Writer.WriteEndElement();
+                        }
                     }
-                }
-
-                Writer.WriteEndElement();
+                });
             }
 
             if (AdditionalFiles.Count != 0)
             {
                 // Additional files
                 Writer.WriteComment("Including additional files");
-                Writer.WriteStartElement("ItemGroup");
-                foreach (string AddFile in AdditionalFiles)
+                WriteElement("ItemGroup", ("Label", "AdditionalFiles"), delegate
                 {
-                    Writer.WriteStartElement("None");
-                    WriteAttribute("Include", AddFile);
-                    Writer.WriteEndElement();
-                }
-
-                Writer.WriteEndElement();
+                    foreach (string AddFile in AdditionalFiles)
+                    {
+                        Writer.WriteStartElement("AdditionalFiles");
+                        WriteAttribute("Include", Path.GetFullPath(AddFile));
+                        Writer.WriteEndElement();
+                    }
+                });
             }
 
 
