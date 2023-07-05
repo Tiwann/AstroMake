faeb046ae08c8b8ec8fbf08f0427b62b01c033d8
Erwann Messoah <tiwannbeats@gmail.com>
2023-07-05 11:41:05 +0200

    Update HelloWorld.Astro.cs

--- Examples/HelloWorld/HelloWorld.Astro.cs
+++ Examples/HelloWorld/HelloWorld.Astro.cs
@@ -3,16 +3,18 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
+
+
 [Build]
 public class HelloWorldSolution : Solution
 {
     public HelloWorldSolution()
     {
         Name = "HelloWorld";
-        Configurations = (List<Configuration>)Configuration.CreateConfigurations("Debug", "Release");
+        Configurations = Configuration.CreateConfigurations("Debug", "Release");
         Architecture = Architecture.x64;
         ProjectNames.Add("HelloWorld");
-        TargetDirectory = Location;
+        TargetDirectory = Path.Combine(Location, "Build");
     }
 }
 
@@ -23,10 +25,10 @@ public class HelloWorldApplication : ConsoleApplication
     {
         Name = "HelloWorld";
         Language = Language.CPlusPlus;
-        Dialect = Dialect.CPP20;
+        CppStandard = CPPStandard.CPP20;
         Flags = ProjectFlags.MultiProcessorCompile;
         Location = Path.Combine(Solution.Location, Name);
-        TargetDirectory = Location;
+        TargetDirectory = Solution.TargetDirectory;
         TargetName = "HelloWorld";
         Files.Add(@"Source\HelloWorld.cpp");
     }
