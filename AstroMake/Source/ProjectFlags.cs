d63fcf0a866340fcd37b9c66012c8038a29969f2
Erwann Messoah <tiwannbeats@gmail.com>
2023-07-06 13:38:19 +0200

    Added some flags

--- AstroMake/Source/ProjectFlags.cs
+++ AstroMake/Source/ProjectFlags.cs
@@ -7,5 +7,8 @@ public enum ProjectFlags
 {
     None = 0,
     MultiProcessorCompile = 1 << 0,
-    ModuleSupport = 1 << 1
+    ModuleSupport = 1 << 1,
+    Optimize = 1 << 2,
+    DebugSymbols = 1 << 3,
+    BuiltInWideCharType = 1 << 4
 }
