7abcba3b7ef87b108aaedd022763dea26d0c249f
Erwann Messoah <tiwannbeats@gmail.com>
2023-07-06 13:37:36 +0200

    Added verbose logs (Might rewrite the API)

--- AstroMake/Source/Log.cs
+++ AstroMake/Source/Log.cs
@@ -4,6 +4,8 @@ namespace AstroMake;
 
 public static class Log
 {
+    public static bool Verbose = false;
+    
     public static void Trace(string Message)
     {
         Console.ForegroundColor = ConsoleColor.White;
@@ -30,5 +32,46 @@ public static class Log
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"{Message}");
         Console.ResetColor();
+    }
+
+    
+    public static void TraceVerbose(string Message)
+    {
+        if (Verbose)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{Message}");
+            Console.ResetColor();
+        }
+    }
+
+    public static void SuccessVerbose(string Message)
+    {
+        if (Verbose)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{Message}");
+            Console.ResetColor();
+        }
+    }
+    
+    public static void WarningVerbose(string Message)
+    {
+        if (Verbose)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{Message}");
+            Console.ResetColor();  
+        }
+    }
+    
+    public static void ErrorVerbose(string Message)
+    {
+        if (Verbose)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Message}");
+            Console.ResetColor();
+        }
     }
 }
