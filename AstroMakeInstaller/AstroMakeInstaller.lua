df0a29493eb3ed35819b58c347a93a228b027fa6
Erwann Messoah <tiwannbeats@gmail.com>
2023-07-11 18:38:52 +0200

    Added icons for the installer

--- AstroMakeInstaller/AstroMakeInstaller.lua
+++ AstroMakeInstaller/AstroMakeInstaller.lua
@@ -5,11 +5,9 @@ project "AstroMakeInstaller"
 	csversion "Latest"
 	targetdir "%{wks.location}/Binaries/%{prj.name}/%{cfg.buildcfg}"
 	objdir "%{wks.location}/Binaries-Intermediate/%{prj.name}/%{cfg.buildcfg}"
-	icon "%{wks.location}/Resources/AstroMakeLogo.ico"
 
 	files {
 		"Source/**.cs",
-		"Properties/AssemblyInfo.cs",
 		"AstroMakeInstaller.lua",
 	}
 	
---
+++ AstroMakeInstaller/Source/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AstroMakeInstaller;
+
+internal static class Program
+{
+
+    public static Uri AstroMakeLatest = new Uri("https://github.com/tiwann/astromake/releases/latest/download/AstroMake-Latest-Windows-x64.exe");
+    
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Astro make installer!");
+        Console.WriteLine("Downloading latest version of Astro Make...");
+
+        using HttpClient Client = new HttpClient();
+        Task<byte[]> File = Client.GetByteArrayAsync(AstroMakeLatest);
+        
+        using FileStream FStream = new FileStream("AstroMake.exe", FileMode.Create, FileAccess.Write);
+        var WriteTask = FStream.WriteAsync(File.Result);
+    }
+}
